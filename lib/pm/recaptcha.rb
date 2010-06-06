b307466eae0cd6d23bd74648f784f06666ea7444
Marcello Barnaba <vjt@openssl.it>
2010-06-06 11:11:46 +0200

    Changed noscript textarea in text input and adjusted the wrapper size

--- lib/pm/recaptcha.rb
+++ lib/pm/recaptcha.rb
@@ -67,9 +67,9 @@ module PM
 
           <noscript>
              <iframe src="https://api-secure.recaptcha.net/noscript?k=#{PublicKey}"
-                 height="200" width="420" frameborder="0"></iframe><br>
-             <textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
-             <input type="hidden" name="recaptcha_response_field" value="manual_challenge">
+                 height="320" width="420" frameborder="0"></iframe><br>
+             <input type="text" class="text" name="recaptcha_challenge_field" tabindex="#{options[:tabindex]}"/>
+             <input type="hidden" name="recaptcha_response_field" value="manual_challenge" />
           </noscript>
         ]
       end
